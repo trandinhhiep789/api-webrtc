31905db0da9012ae48c945ec31ec891a3060dab1
HiepTran <trandinhhiep789@gmail.com>
2022-02-22 13:30:54 +0700

    fix: add PORT for deploy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var io = require("socket.io")({
 
 const app = express();
 app.use(cors())
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 // chạy front end
 app.use(express.static(__dirname + "/build"));
